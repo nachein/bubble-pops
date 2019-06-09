ca0b6729b37169e47a3119344b6e42fbb71d59af
nachein <ignaciocasal@gmail.com>
2019-06-09 18:05:45 +0200

    Add Bubble config scriptable object

---
+++ Assets/Scripts/BubblePops/ScriptableObjects/BubbleConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BubblePops.ScriptableObjects
+{
+	[CreateAssetMenu(fileName = "BubbleConfig", menuName = "ScriptableObjects/BubbleConfig", order = 1)]
+	public class BubbleConfig : ScriptableObject
+	{
+		public List<BubbleConfigItem> configs;
+	}	
+}
---
+++ Assets/Scripts/BubblePops/ScriptableObjects/BubbleConfigItem.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using UnityEngine;
+
+namespace BubblePops.ScriptableObjects 
+{
+	[Serializable]
+	public class BubbleConfigItem
+	{
+		public int number;
+		public Color32 color;
+	}
+}
